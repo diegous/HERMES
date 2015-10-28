249adeaa0c35bfb1c02c9664d8fc228fb2508254
carolina <mariacarola46@hotmail.com>
2015-10-28 11:46:30 -0300

    retoque posicion filtros

--- src/view/HermesView.java
+++ src/view/HermesView.java
@@ -70,7 +70,7 @@ public class HermesView extends JFrame {
 	 */
 	public HermesView() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(50, 50, 800, 500);
+		setBounds(50, 50, 900, 600);
 		setTitle("Hermes Monitor");
 		contentPane = new JPanel();
 		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
@@ -81,6 +81,8 @@ public class HermesView extends JFrame {
 		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
+		
+		
 		
 		
 		
@@ -95,10 +97,10 @@ public class HermesView extends JFrame {
 		gbc_Filtros.gridy = 0;
 		contentPane.add(Filtros, gbc_Filtros);
 		GridBagLayout gbl_Filtros = new GridBagLayout();
-		gbl_Filtros.columnWidths = new int[]{0, 0, 0, 0};
+		gbl_Filtros.columnWidths = new int[]{50, 100, 100};
 		gbl_Filtros.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gbl_Filtros.columnWeights = new double[]{0.0, 0.1, 1.0, 0.0};
-		gbl_Filtros.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0};
+		gbl_Filtros.columnWeights = new double[]{1.0, 1.0, 1.0};
+		gbl_Filtros.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
 		Filtros.setLayout(gbl_Filtros);
 		
 		
@@ -120,12 +122,13 @@ public class HermesView extends JFrame {
 		filtros.setForeground(new Color(0, 0, 0));
 		filtros.setFont(new Font("Cambria", Font.BOLD, 15));
 		
+		//CONTENIDO
 		JLabel contenido = new JLabel("Contenido:");
 		contenido.setHorizontalAlignment(SwingConstants.LEFT);
-		contenido.setFont(new Font("Cambria", Font.BOLD, 11));
+		//contenido.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_contenido = new GridBagConstraints();
 		gbc_contenido.fill = GridBagConstraints.HORIZONTAL;
-		gbc_contenido.insets = new Insets(0, 0, 5, 5);
+		gbc_contenido.insets = new Insets(0, 5, 5, 5);
 		gbc_contenido.gridx = 0;
 		gbc_contenido.gridy = 1;
 		Filtros.add(contenido, gbc_contenido);
@@ -139,22 +142,61 @@ public class HermesView extends JFrame {
 		gbc_comboBox.gridy = 1;
 		Filtros.add(comboBox, gbc_comboBox);
 		
+		//FECHA
 		JLabel Fechahora = new JLabel("Fecha/Hora");
 		Fechahora.setHorizontalAlignment(SwingConstants.LEFT);
-		Fechahora.setFont(new Font("Cambria", Font.BOLD, 11));
+		//Fechahora.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_Fechahora = new GridBagConstraints();
-		gbc_Fechahora.gridwidth = 2;
-		gbc_Fechahora.insets = new Insets(0, 0, 5, 0);
+		gbc_Fechahora.insets = new Insets(0, 10, 0, 10);
 		gbc_Fechahora.gridx = 2;
 		gbc_Fechahora.gridy = 1;
 		Filtros.add(Fechahora, gbc_Fechahora);
 		
+				
+		JLabel desde = new JLabel("Desde:");
+		//desde.setFont(new Font("Cambria", Font.BOLD, 11));
+		GridBagConstraints gbc_desde = new GridBagConstraints();
+		gbc_desde.fill = GridBagConstraints.HORIZONTAL;
+		gbc_desde.insets = new Insets(0, 10, 0, 10);
+		gbc_desde.gridx = 2;
+		gbc_desde.gridy = 2;
+		Filtros.add(desde, gbc_desde);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setModel(new SpinnerDateModel(new Date(1445914800000L), new Date(1445914800000L), new Date(1476932400000L), Calendar.DAY_OF_YEAR));
+		GridBagConstraints gbc_spinner = new GridBagConstraints();
+		gbc_spinner.insets = new Insets(0, 10, 0, 10);
+		gbc_spinner.gridx = 2;
+		gbc_spinner.gridy = 3;
+		Filtros.add(spinner, gbc_spinner);
+		
+		JLabel hasta = new JLabel("Hasta:");
+		//hasta.setFont(new Font("Cambria", Font.BOLD, 11));
+		GridBagConstraints gbc_hasta = new GridBagConstraints();
+		gbc_hasta.fill = GridBagConstraints.HORIZONTAL;
+		gbc_hasta.insets = new Insets(0,10,0, 10);
+		gbc_hasta.gridx = 2;
+		gbc_hasta.gridy = 4;
+		Filtros.add(hasta, gbc_hasta);
+		
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setModel(new SpinnerDateModel(new Date(1445914800000L), new Date(1445914800000L), new Date(1445914800000L), Calendar.DAY_OF_YEAR));
+		GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
+		gbc_spinner_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_spinner_1.insets = new Insets(0, 10, 0, 10);
+		gbc_spinner_1.gridx = 2;
+		gbc_spinner_1.gridy = 5;
+		Filtros.add(spinner_1, gbc_spinner_1);
+		
+		
+		//CONTEXTO
 		JLabel contexto = new JLabel("Contexto:");
 		contexto.setHorizontalAlignment(SwingConstants.LEFT);
-		contexto.setFont(new Font("Cambria", Font.BOLD, 11));
+		//contexto.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_contexto = new GridBagConstraints();
 		gbc_contexto.fill = GridBagConstraints.HORIZONTAL;
-		gbc_contexto.insets = new Insets(0, 0, 5, 5);
+		gbc_contexto.insets = new Insets(0, 5, 5, 5);
 		gbc_contexto.gridx = 0;
 		gbc_contexto.gridy = 2;
 		Filtros.add(contexto, gbc_contexto);
@@ -168,29 +210,13 @@ public class HermesView extends JFrame {
 		gbc_comboBox_1.gridy = 2;
 		Filtros.add(comboBox_1, gbc_comboBox_1);
 		
-		JLabel desde = new JLabel("Desde:");
-		desde.setFont(new Font("Cambria", Font.BOLD, 11));
-		GridBagConstraints gbc_desde = new GridBagConstraints();
-		gbc_desde.fill = GridBagConstraints.HORIZONTAL;
-		gbc_desde.insets = new Insets(0, 0, 5, 5);
-		gbc_desde.gridx = 2;
-		gbc_desde.gridy = 2;
-		Filtros.add(desde, gbc_desde);
-		
-		JSpinner spinner = new JSpinner();
-		spinner.setModel(new SpinnerDateModel(new Date(1445914800000L), new Date(1445914800000L), new Date(1476932400000L), Calendar.DAY_OF_YEAR));
-		GridBagConstraints gbc_spinner = new GridBagConstraints();
-		gbc_spinner.insets = new Insets(0, 0, 5, 0);
-		gbc_spinner.gridx = 3;
-		gbc_spinner.gridy = 2;
-		Filtros.add(spinner, gbc_spinner);
-		
+		//CATEGORIA
 		JLabel categoria = new JLabel("Categor\u00EDa:");
 		categoria.setHorizontalAlignment(SwingConstants.LEFT);
-		categoria.setFont(new Font("Cambria", Font.BOLD, 11));
+		//categoria.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_categoria = new GridBagConstraints();
 		gbc_categoria.fill = GridBagConstraints.HORIZONTAL;
-		gbc_categoria.insets = new Insets(0, 0, 5, 5);
+		gbc_categoria.insets = new Insets(0, 5, 5, 5);
 		gbc_categoria.gridx = 0;
 		gbc_categoria.gridy = 3;
 		Filtros.add(categoria, gbc_categoria);
@@ -204,30 +230,15 @@ public class HermesView extends JFrame {
 		gbc_comboBox_2.gridy = 3;
 		Filtros.add(comboBox_2, gbc_comboBox_2);
 		
-		JLabel hasta = new JLabel("Hasta:");
-		hasta.setFont(new Font("Cambria", Font.BOLD, 11));
-		GridBagConstraints gbc_hasta = new GridBagConstraints();
-		gbc_hasta.fill = GridBagConstraints.HORIZONTAL;
-		gbc_hasta.insets = new Insets(0, 0, 5, 5);
-		gbc_hasta.gridx = 2;
-		gbc_hasta.gridy = 3;
-		Filtros.add(hasta, gbc_hasta);
 		
-		JSpinner spinner_1 = new JSpinner();
-		spinner_1.setModel(new SpinnerDateModel(new Date(1445914800000L), new Date(1445914800000L), new Date(1445914800000L), Calendar.DAY_OF_YEAR));
-		GridBagConstraints gbc_spinner_1 = new GridBagConstraints();
-		gbc_spinner_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_spinner_1.insets = new Insets(0, 0, 5, 0);
-		gbc_spinner_1.gridx = 3;
-		gbc_spinner_1.gridy = 3;
-		Filtros.add(spinner_1, gbc_spinner_1);
 		
+		//NINO
 		JLabel nino = new JLabel("Ni\u00F1@:");
 		nino.setHorizontalAlignment(SwingConstants.LEFT);
-		nino.setFont(new Font("Cambria", Font.BOLD, 11));
+		//nino.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_nino = new GridBagConstraints();
 		gbc_nino.fill = GridBagConstraints.HORIZONTAL;
-		gbc_nino.insets = new Insets(0, 0, 5, 5);
+		gbc_nino.insets = new Insets(0, 5, 5, 5);
 		gbc_nino.gridx = 0;
 		gbc_nino.gridy = 4;
 		Filtros.add(nino, gbc_nino);
@@ -241,9 +252,13 @@ public class HermesView extends JFrame {
 		gbc_comboBox_3.gridy = 4;
 		Filtros.add(comboBox_3, gbc_comboBox_3);
 		
+		
+		
+		
+		//ETIQUETA
 		JLabel Etiqueta = new JLabel("Etiqueta:");
 		Etiqueta.setHorizontalAlignment(SwingConstants.LEFT);
-		Etiqueta.setFont(new Font("Cambria", Font.BOLD, 11));
+		//Etiqueta.setFont(new Font("Cambria", Font.BOLD, 11));
 		GridBagConstraints gbc_Etiqueta = new GridBagConstraints();
 		gbc_Etiqueta.fill = GridBagConstraints.HORIZONTAL;
 		gbc_Etiqueta.insets = new Insets(0, 0, 5, 5);
@@ -259,6 +274,8 @@ public class HermesView extends JFrame {
 		gbc_comboBox_4.gridx = 1;
 		gbc_comboBox_4.gridy = 5;
 		Filtros.add(comboBox_4, gbc_comboBox_4);
+		
+		
 		
 		JButton btnNewButton = new JButton("Filtrar");
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
@@ -277,17 +294,20 @@ public class HermesView extends JFrame {
 		Etiquetas.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		Etiquetas.setBackground(new Color(153, 204, 51));
 		GridBagConstraints gbc_Etiquetas = new GridBagConstraints();
-		gbc_Etiquetas.insets = new Insets(5,5, 5, 5);
+		gbc_Etiquetas.insets = new Insets(5, 5, 5, 5);
 		gbc_Etiquetas.fill = GridBagConstraints.BOTH;
 		gbc_Etiquetas.gridx = 1;
 		gbc_Etiquetas.gridy = 0;
 		contentPane.add(Etiquetas, gbc_Etiquetas);
 		GridBagLayout gbl_Etiquetas = new GridBagLayout();
-		gbl_Etiquetas.columnWidths = new int[]{0, 0, 0};
+		gbl_Etiquetas.columnWidths = new int[]{50, 100, 50};
 		gbl_Etiquetas.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_Etiquetas.columnWeights = new double[]{1.0, 1.0, 1.0,Double.MIN_VALUE};
 		gbl_Etiquetas.rowWeights = new double[]{1.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0};
 		Etiquetas.setLayout(gbl_Etiquetas);
+		
+		
+		
 		
 		
 		//TITULO
@@ -297,7 +317,7 @@ public class HermesView extends JFrame {
 		panel.setBackground(new Color(51, 102, 153));
 		gbc_panel.anchor = GridBagConstraints.NORTH;
 		gbc_panel.gridwidth = 4;
-		gbc_panel.insets = new Insets(0, 0, 10, 0);
+		gbc_panel.insets = new Insets(0, 0, 0, 0);
 		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
 		gbc_panel.gridx = 0;
 		gbc_panel.gridy = 0;
@@ -312,7 +332,7 @@ public class HermesView extends JFrame {
 		JLabel lblNewLabel = new JLabel("Crear etiqueta:");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
 		gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.insets = new Insets(0, 5, 5, 5);
 		gbc_lblNewLabel.gridx = 0;
 		gbc_lblNewLabel.gridy = 1;
 		Etiquetas.add(lblNewLabel, gbc_lblNewLabel);
@@ -335,8 +355,8 @@ public class HermesView extends JFrame {
 		Etiquetas.add(btnCrear, gbc_btnCrear);
 		
 		JSeparator separator = new JSeparator();
-		separator.setBackground(new Color(192, 192, 192));
-		separator.setForeground(new Color(112, 128, 144));
+		separator.setBackground(Color.BLACK);
+		separator.setForeground(Color.BLACK);
 		GridBagConstraints gbc_separator = new GridBagConstraints();
 		gbc_separator.fill = GridBagConstraints.HORIZONTAL;
 		gbc_separator.gridwidth = 4;
@@ -350,7 +370,7 @@ public class HermesView extends JFrame {
 		JLabel lblNewLabel_1 = new JLabel("Eliminar etiqueta:");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_1.insets = new Insets(0, 5, 5, 5);
 		gbc_lblNewLabel_1.gridx = 0;
 		gbc_lblNewLabel_1.gridy = 3;
 		Etiquetas.add(lblNewLabel_1, gbc_lblNewLabel_1);
@@ -372,8 +392,8 @@ public class HermesView extends JFrame {
 		Etiquetas.add(btnEliminar, gbc_btnEliminar);
 		
 		JSeparator separator_1 = new JSeparator();
-		separator_1.setForeground(new Color(95, 158, 160));
-		separator_1.setBackground(new Color(0, 0, 128));
+		separator_1.setForeground(Color.BLACK);
+		separator_1.setBackground(Color.BLACK);
 		GridBagConstraints gbc_separator_1 = new GridBagConstraints();
 		gbc_separator_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_separator_1.gridwidth = 4;
@@ -387,7 +407,7 @@ public class HermesView extends JFrame {
 		JLabel lblNewLabel_2 = new JLabel("Asignacion etiqueta:");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.WEST;
-		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_2.insets = new Insets(0, 5, 5, 5);
 		gbc_lblNewLabel_2.gridx = 0;
 		gbc_lblNewLabel_2.gridy = 5;
 		Etiquetas.add(lblNewLabel_2, gbc_lblNewLabel_2);
@@ -409,6 +429,8 @@ public class HermesView extends JFrame {
 		Etiquetas.add(btnAsignardesasignar, gbc_btnAsignardesasignar);
 		
 		JSeparator separator_2 = new JSeparator();
+		separator_2.setForeground(Color.BLACK);
+		separator_2.setBackground(Color.BLACK);
 		GridBagConstraints gbc_separator_2 = new GridBagConstraints();
 		gbc_separator_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_separator_2.gridwidth = 4;
@@ -423,7 +445,7 @@ public class HermesView extends JFrame {
 		JLabel lblRenombrarEtiqueta = new JLabel("Renombrar etiqueta:");
 		GridBagConstraints gbc_lblRenombrarEtiqueta = new GridBagConstraints();
 		gbc_lblRenombrarEtiqueta.anchor = GridBagConstraints.WEST;
-		gbc_lblRenombrarEtiqueta.insets = new Insets(0, 0, 5, 5);
+		gbc_lblRenombrarEtiqueta.insets = new Insets(0, 5, 5, 5);
 		gbc_lblRenombrarEtiqueta.gridx = 0;
 		gbc_lblRenombrarEtiqueta.gridy = 7;
 		Etiquetas.add(lblRenombrarEtiqueta, gbc_lblRenombrarEtiqueta);
@@ -432,6 +454,7 @@ public class HermesView extends JFrame {
 		GridBagConstraints gbc_comboBox_7 = new GridBagConstraints();
 		gbc_comboBox_7.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox_7.insets = new Insets(0, 0, 5, 5);
+		gbc_comboBox_7.gridwidth = 2;
 		gbc_comboBox_7.gridx = 1;
 		gbc_comboBox_7.gridy = 7;
 		Etiquetas.add(comboBox_7, gbc_comboBox_7);
@@ -439,7 +462,7 @@ public class HermesView extends JFrame {
 		JLabel lblNuevoNombre = new JLabel("Nuevo nombre:");
 		GridBagConstraints gbc_lblNuevoNombre = new GridBagConstraints();
 		gbc_lblNuevoNombre.anchor = GridBagConstraints.WEST;
-		gbc_lblNuevoNombre.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNuevoNombre.insets = new Insets(0, 5, 0, 5);
 		gbc_lblNuevoNombre.gridx = 0;
 		gbc_lblNuevoNombre.gridy = 8;
 		Etiquetas.add(lblNuevoNombre, gbc_lblNuevoNombre);
@@ -479,9 +502,9 @@ public class HermesView extends JFrame {
 		gbc_Notificaciones.gridy = 1;
 		contentPane.add(Notificaciones, gbc_Notificaciones);
 		GridBagLayout gbl_Notificaciones = new GridBagLayout();
-		gbl_Notificaciones.columnWidths = new int[]{0, };
+		gbl_Notificaciones.columnWidths = new int[]{0};
 		gbl_Notificaciones.rowHeights = new int[]{0, 0};
-		gbl_Notificaciones.columnWeights = new double[]{0.0, 1.0};
+		gbl_Notificaciones.columnWeights = new double[]{1.0};
 		gbl_Notificaciones.rowWeights = new double[]{0.0, 1.0};
 		Notificaciones.setLayout(gbl_Notificaciones);
 		
@@ -536,9 +559,10 @@ public class HermesView extends JFrame {
 			
 			JScrollPane scrollPane = new JScrollPane(table);
 			GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+			gbc_scrollPane.gridwidth = 3;
 			gbc_scrollPane.insets = new Insets(0, 0, 0, 0);
 			gbc_scrollPane.fill = GridBagConstraints.BOTH;
-			gbc_scrollPane.gridx = 1;
+			gbc_scrollPane.gridx = 0;
 			gbc_scrollPane.gridy = 1;
 			Notificaciones.add(scrollPane, gbc_scrollPane);
 		
