8845c986a72c81957d83ffc2c10f4c2807ae5094
diegous <munoa.d@gmail.com>
2015-11-02 13:17:45 -0300

    Agrego el archivo de jsons

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,27 +1,43 @@
 package controller;
 
 import java.awt.EventQueue;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import modelo.*;
 import view.HermesView;
 
 public class MainController {
+	
+	public static List<String> getJson(){
+		List<String> result = new ArrayList<String>();
+		try (BufferedReader br = new BufferedReader(new FileReader("jsons.json"))) {
+		    String line;
+		    while ((line = br.readLine()) != null) {
+		       result.add(line);
+		    }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
 
 	
 	public static void main(String[] args){
-		
-		String json = "{"
-				+ "\"child\": \"pepito2\", "
-				+ "\"context\":\"Pista\", "
-				+ "\"category\":\"Emociones\", "
-				+ "\"sent\":1111111111111111111, "
-				+ "\"tag\":\"infeliz\", "
-				+ "\"content\":\"Alegre\"}";
-		
 		Controller controller = new Controller();
-		controller.processIncomingJson(json);
-		
-		
+
+		for(String json : getJson()){
+			System.out.println(json);
+			//controller.processIncomingJson(json);
+		}
 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
