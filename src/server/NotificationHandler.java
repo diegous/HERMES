c26df75724bdf0993d27cc16aa9ceec350a53704
carolina <mariacarola46@hotmail.com>
2015-11-19 23:24:09 -0300

    detalle visualizacion

--- src/server/NotificationHandler.java
+++ src/server/NotificationHandler.java
@@ -11,7 +11,7 @@ public class NotificationHandler implements HttpHandler {
 	@Override
 	public void handle(HttpExchange exchange) throws IOException {
 		
-		/*
+		
 		InputStream is = exchange.getRequestBody();
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
 		String inputLine;
@@ -19,13 +19,16 @@ public class NotificationHandler implements HttpHandler {
 
 		
 		while ((inputLine = bufferedReader.readLine()) != null) {
+			System.out.println(inputLine);
 			stringBuffer.append(inputLine);
 		}
 		bufferedReader.close();
-*/
-		System.out.println("oooo");
+
 		
 		
+		
+		
+		//RESPUESTA
 		String response = "OK";
 		exchange.sendResponseHeaders(200, (long)response.length());
 		OutputStream os = exchange.getResponseBody();
