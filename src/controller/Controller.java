c2550d8eb48b8e2059dec1ebd6fdd1fd40290ca3
diegous <munoa.d@gmail.com>
2015-11-21 20:17:38 -0300

    Agregado archivo XML de configuración con número de puerto. El server escucha con un pool the threads(?) como pide el enunciado.

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,10 @@
 package controller;
 
 import java.util.Date;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+import org.json.simple.JSONValue;
 import entities.Category;
 import entities.Child;
 import entities.Context;
@@ -13,36 +14,32 @@ import modelo.*;
 
 public class Controller {
 	public void processIncomingJson(String json, SynchronizerNotifications sn){
-		JSONParser parser = new JSONParser();
+		JSONArray jArray = (JSONArray) JSONValue.parse(json);
 		
-		try {
-			JSONObject jArray = (JSONObject) parser.parse(json);
+		for(int i = 0; i<jArray.size(); i++){
+			JSONObject jObject = (JSONObject) jArray.get(i);
 			
 			String
-				jchild     = (String) jArray.get("child"),
-				jcontext   = (String) jArray.get("context"),
-				jcategory  = (String) jArray.get("category"),
-				jpictogram = (String) jArray.get("pictogram");
+				jchild     = (String) jObject.get("child"),
+				jcontext   = (String) jObject.get("context"),
+				jcategory  = (String) jObject.get("category"),
+				jpictogram = (String) jObject.get("pictogram");
 			
 			long
-				jsent      = (Long) jArray.get("sent");
-				
+				jsent      = (Long) jObject.get("sent");
 
-			
 			Child child = FactoriaDAO.getChildDAO().getByText(jchild);
 			if (child == null){
 				FactoriaDAO.getChildDAO().save(new Child(-1, jchild));
 				child = FactoriaDAO.getChildDAO().getByText(jchild);
 				sn.getChild().addElement(child);
-				
 			}
 
 			Context context = FactoriaDAO.getContextDAO().getByText(jcontext);
 			if (context == null){
 				FactoriaDAO.getContextDAO().save(new Context(-1, jcontext));
 				context = FactoriaDAO.getContextDAO().getByText(jcontext);
-				sn.getContext().addElement(context);
-				
+				sn.getContext().addElement(context);	
 			}
 
 			Category category = FactoriaDAO.getCategoryDAO().getByText(jcategory);
@@ -50,7 +47,6 @@ public class Controller {
 				FactoriaDAO.getCategoryDAO().save(new Category(-1, jcategory));
 				category = FactoriaDAO.getCategoryDAO().getByText(jcategory);
 				sn.getCategory().addElement(category);
-				
 			}
 
 			Pictogram pictogram = FactoriaDAO.getPictogramDAO().getByText(jpictogram);
@@ -58,7 +54,6 @@ public class Controller {
 				FactoriaDAO.getPictogramDAO().save(new Pictogram(-1, jpictogram));
 				pictogram = FactoriaDAO.getPictogramDAO().getByText(jpictogram);
 				sn.getPictogram().addElement(pictogram);
-				
 			}
 			
 			Date sent = new Date(jsent);
@@ -66,9 +61,6 @@ public class Controller {
 			Notification notification = new Notification(-1,child,context,category, null,pictogram,sent,recieved);
 			FactoriaDAO.getNotificationDAO().save(notification);
 			sn.getNotification().addElement(notification);
-		} catch (ParseException e) {
-			e.printStackTrace();
 		}
-		
 	}
 }
