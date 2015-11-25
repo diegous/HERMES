38543b4cfa9420decabeca02e3602044561ea319
carolina <mariacarola46@hotmail.com>
2015-11-25 14:00:26 -0300

    filtrar por un solo dia en particular

--- src/modelo/NotificationDAO.java
+++ src/modelo/NotificationDAO.java
@@ -71,7 +71,7 @@ public class NotificationDAO implements INotificationDAO {
 	      
 	        if(f.getSince() != 0){sql=sql+" and sent_date>=?";}
 	        //La fecha "hasta" va siempre
-	        sql=sql+" and received_date<=?";
+	        sql=sql+" and sent_date<=?";
 	        
 	        sql=sql+" GROUP BY n.id_notification";
 	        sql=sql+" ORDER BY n.sent_date DESC";
